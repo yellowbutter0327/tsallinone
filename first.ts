a7496ee2d36b506b5d7ce41ec487c9a99734995d
yellowbutter0327 <slrkwhdmsp@naver.com>
2024-02-12 22:36:32 +0900

    FEAT: never 타입과 느낌표(non-null assertion) / 원시 래퍼 타입, 템플릿 리터럴 타입, rest, 튜플 / enum,keyof,typeof / union(|)과 intersection(&)

--- first.ts
+++ first.ts
@@ -84,3 +84,95 @@ try {
 } catch (error) {
   error;
 }
+
+//뒤에 붙이는 느낌표는 여기서 head는 Element랑 null 둘 다 오는데
+//여기 무조건 Element 인 것을 책임질 수 있으면 '!'를 붙인다.
+//null이나 undefined가 아님을 보증하는 것이다.
+//이 방법은 비추한다. 절대는 없기 때문이다.
+const head: Element = document.querySelector("#head")!;
+//그냥 head.innerHTMl 이렇게 하면 에러가 날 수 있기 때문에
+//if(head)이런 식으로 적어주는게 좋다.
+if (head) {
+  head.innerHTML = "hello world";
+}
+
+//1.5. 원시 래퍼 타입, 템플릿 리터럽 타입, rest, 튜플
+type World = "world" | "hell";
+const word: World = "world";
+type World2 = `hello ${World}`;
+//이렇게 string이 들어가면 추천이 안된다.
+type World3 = `hello ${string}`;
+//추천 두개 뜨는거 보이지
+const word3: World2 = `hello hell`;
+
+//rest parameter
+function rest(a, ...args: string[]) {
+  console.log(args); //(1,[2,3])
+}
+
+rest("1", "2", "3");
+
+//tuple
+const tuple: [string, number] = ["1", 5];
+// tuple[2] = 4 typescript가 얘는 막는데
+//tuple.push(2) //얘는 못 막아준다.
+
+//1.6. Enum
+//변수들을 하나의 그룹으로 묶고 싶을때 Enum이라는 것을 쓴다.
+//위에서 부터 순서대ㅗㄹ 0,1,2,3 이라는 값이 부여된다.
+
+//남아있을지, 안 남아있을지 고민이라면 남겨라
+//javascript에서 남아있게 하고 싶으면
+//위에 ODirection:{Up:1,Down:2...} 이런식으로
+//할 수도 있지만 as const 붙이면 된다.
+const ODirection = {
+  Up: 1,
+  Down: 2,
+  Left: 3,
+  Right: 4,
+} as const;
+
+//javascript에서 안 남아있어도 되면
+const enum EDirection {
+  Up,
+  Down,
+  Left,
+  Right,
+}
+//const a = EDirection.Up;
+//const c = EDirection.Left;
+
+//Enum 사용할때
+//dir은 저 안에있는 것 중에 사용하겠다.
+//function walk(dir:Edirction){}
+
+//keyof
+const obj4 = { a: 123, b: "hello" } as const;
+//여기서 a,b,c만 꺼내오고 싶을때
+//값을 typeof로 쓰고 싶을때 typeof를 붙여준다.
+// obj는 js 값 이므로 타입으로 쓸 수 없음
+//typeof만 붙이면 {a:string, b:string}이건데
+// keyof 까지 붙이면 {a,b}이렇게 된다.
+type Key = keyof typeof obj4; //key값 가져옴
+type Key2 = (typeof obj4)[keyof typeof obj4]; //obj값 가져옴
+
+//1.7.union(|)과 intersection(&)
+//간단하게 할 때는 type을 사용하고
+//객체지향 프로그래밍을 하고 싶을때는 interface를 사용하자.
+type A = { a: string };
+const a5: A = { a: "hello" };
+
+interface B {
+  b: string;
+}
+const a6: B = { b: "hi" };
+
+//union
+type A7 = string | number;
+
+//intersection(&)
+// type A8 = string & number; 이런 것은 불가능할 것
+type intersect = { name: "Eunhye" } & { age: 25 };
+//또는 이면 밑에 exmaple에 속성 한 개만 있어도 된다.
+type intersect2 = { name: "Eunhye" } | { age: 25 };
+const intersectexample = { name: "Eunhye", age: 25 };
